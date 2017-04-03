9bb246c3f8995bf2fdafdd8574ee68c7a3a05ea4
Julien Billoudet <julien.billoudet@gmail.com>
2017-04-03 20:44:31 +0200

    Adding infrastructure to use Paypal Button Manager SDK

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -25,6 +25,7 @@ class AppKernel extends Kernel
             new Ivory\OrderedFormBundle\IvoryOrderedFormBundle(),
             new AppBundle\AppBundle(),
             new GS\ApiBundle\GSApiBundle(),
+            new GS\PaypalBMBridgeBundle\GSPaypalBMBridgeBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
---
+++ src/GS/PaypalBMBridgeBundle/GSPaypalBMBridgeBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace GS\PaypalBMBridgeBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class GSPaypalBMBridgeBundle extends Bundle
+{
+}
