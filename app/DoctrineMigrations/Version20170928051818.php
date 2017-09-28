7e98ee403c16668dc1fb97e350fa81a6cb7d4728
Julien Billoudet <julien.billoudet@gmail.com>
2017-09-28 07:22:07 +0200

    Adding file to certificates

---
+++ app/DoctrineMigrations/Version20170928051818.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerAwareTrait;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20170928051818 extends AbstractMigration implements ContainerAwareInterface
+{
+    use ContainerAwareTrait;
+
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        if ( $this->container->get('kernel')->getEnvironment() == "dev" ) {
+            $this->addSql('ALTER TABLE test_api_certificate ADD updated_at DATETIME NOT NULL, ADD file_name VARCHAR(255) DEFAULT NULL');
+        } elseif ( $this->container->get('kernel')->getEnvironment() == "prod" ) {
+            $this->addSql('ALTER TABLE gs_api_certificate ADD updated_at DATETIME NOT NULL, ADD file_name VARCHAR(255) DEFAULT NULL');
+        }
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        if ( $this->container->get('kernel')->getEnvironment() == "dev" ) {
+            $this->addSql('ALTER TABLE test_api_certificate DROP updated_at, DROP file_name');
+        } elseif ( $this->container->get('kernel')->getEnvironment() == "prod" ) {
+            $this->addSql('ALTER TABLE gs_api_certificate DROP updated_at, DROP file_name');
+        }
+    }
+}
--- src/GS/ApiBundle/Entity/Certificate.php
+++ src/GS/ApiBundle/Entity/Certificate.php
@@ -3,8 +3,11 @@
 namespace GS\ApiBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Mapping\Annotation as Gedmo;
 use JMS\Serializer\Annotation\Type;
+use Symfony\Component\HttpFoundation\File\File;
 use Symfony\Component\Validator\Constraints as Assert;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
 
 /**
  * Certificate
@@ -54,6 +57,32 @@ class Certificate
      * @Type("Relation")
      */
     private $year;
+
+    /**
+     * @ORM\Column(type="datetime")
+     * @Gedmo\Timestampable(on="update")
+     *
+     * @var \DateTime
+     */
+    private $updatedAt;
+
+    /**
+     * NOTE: This is not a mapped field of entity metadata, just a simple property.
+     *
+     * @Vich\UploadableField(mapping="certificate_file", fileNameProperty="fileName")
+     * @Assert\File(
+     *     maxSize = "3M",
+     * )
+     * @var File
+     */
+    private $file;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     *
+     * @var string
+     */
+    private $fileName;
 
 
     /**
@@ -196,4 +225,99 @@ class Certificate
     {
         return $this->year;
     }
+
+    /**
+     * Get display
+     *
+     * @return string
+     */
+    public function getDisplay()
+    {
+        if ( $this->getType() == 'student' ) {
+            return $this->getAccount()->getDisplayName() . ' - étudiant';
+        } else {
+            return $this->getAccount()->getDisplayName() . ' - chômeur';
+        }
+    }
+
+    /**
+     * Set updatedAt
+     *
+     * @param \DateTime $updatedAt
+     *
+     * @return Certificate
+     */
+    public function setUpdatedAt($updatedAt)
+    {
+        $this->updatedAt = $updatedAt;
+
+        return $this;
+    }
+
+    /**
+     * Get updatedAt
+     *
+     * @return \DateTime
+     */
+    public function getUpdatedAt()
+    {
+        return $this->updatedAt;
+    }
+
+    /**
+     * Set fileName
+     *
+     * @param string $fileName
+     *
+     * @return Certificate
+     */
+    public function setFileName($fileName)
+    {
+        $this->fileName = $fileName;
+
+        return $this;
+    }
+
+    /**
+     * Get fileName
+     *
+     * @return string
+     */
+    public function getFileName()
+    {
+        return $this->fileName;
+    }
+
+    /**
+     * If manually uploading a file (i.e. not using Symfony Form) ensure an instance
+     * of 'UploadedFile' is injected into this setter to trigger the  update. If this
+     * bundle's configuration parameter 'inject_on_load' is set to 'true' this setter
+     * must be able to accept an instance of 'File' as the bundle will inject one here
+     * during Doctrine hydration.
+     *
+     * @param File|\Symfony\Component\HttpFoundation\File\UploadedFile $file
+     *
+     * @return Product
+     */
+    public function setFile(File $file = null)
+    {
+        $this->file = $file;
+
+        if ($file) {
+            // It is required that at least one field changes if you are using doctrine
+            // otherwise the event listeners won't be called and the file is lost
+            $this->updatedAt = new \DateTimeImmutable();
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return File|null
+     */
+    public function getFile()
+    {
+        return $this->imageFile;
+    }
+
 }
