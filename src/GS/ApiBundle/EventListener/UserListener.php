b44db976001290b336ff00c589732fb9d7f95083
Julien Billoudet <julien.billoudet@gmail.com>
2017-10-11 13:50:06 +0200

    Correcting bug with password reset

--- src/GS/ApiBundle/EventListener/UserListener.php
+++ src/GS/ApiBundle/EventListener/UserListener.php
@@ -6,6 +6,7 @@ use FOS\UserBundle\Event\FormEvent;
 use FOS\UserBundle\FOSUserEvents;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 class UserListener implements EventSubscriberInterface
@@ -24,11 +25,17 @@ class UserListener implements EventSubscriberInterface
     {
         return array(
             FOSUserEvents::CHANGE_PASSWORD_SUCCESS => 'onChangePasswordSuccess',
+            FOSUserEvents::RESETTING_RESET_SUCCESS => 'onResettingPasswordSuccess',
         );
     }
 
     public function onChangePasswordSuccess(FormEvent $event)
     {
         $event->setResponse(new JsonResponse(null, 204));
+    }
+
+    public function onResettingPasswordSuccess(FormEvent $event)
+    {
+        $event->setResponse(new RedirectResponse('https://inscriptions.grenobleswing.com/'));
     }
 }
